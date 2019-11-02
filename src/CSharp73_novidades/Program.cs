9148988574564bf263aa9489230ace3d37436a87
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2019-11-02 13:42:45 -0300

    Video 1.2

---
+++ src/CSharp73_novidades/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp73_novidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesteTuples.TestaTuple();
+
+
+            Console.WriteLine("Fim de execução");
+        }
+    }
+}
