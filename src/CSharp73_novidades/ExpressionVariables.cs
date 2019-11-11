1704b07e0ee527fd3c424864152cc5b533a70bbe
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2019-11-11 20:48:25 -0300

    Video 2.1

---
+++ src/CSharp73_novidades/ExpressionVariables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp73_novidades
+{
+    class AplicacaoWeb
+    {
+        public static string Porta = "8080";
+
+        public static bool EstaRodandoEmPortaAlta =
+            int.TryParse(Porta, out int portaComoInt) && portaComoInt > 1024;
+
+    }
+
+
+
+    class ExpressionVariables
+    {
+        public bool ValidaIdade(string idadeComoTexto) =>
+            int.TryParse(idadeComoTexto, out int idade) && idade > 18;
+
+        public static void TestaExpressionVariables()
+        {
+            var idadeComoTexto = "15";
+
+            
+            if(int.TryParse(idadeComoTexto, out int idade) && idade > 18)
+            {
+                Console.WriteLine("Voce pode entrar");
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
