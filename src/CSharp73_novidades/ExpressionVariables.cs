d4914f2a25db999c589e97f3df33e2eeeab0f164
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2019-11-11 21:25:38 -0300

    Video 2.2

--- src/CSharp73_novidades/ExpressionVariables.cs
+++ src/CSharp73_novidades/ExpressionVariables.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,10 +11,7 @@ namespace CSharp73_novidades
 
         public static bool EstaRodandoEmPortaAlta =
             int.TryParse(Porta, out int portaComoInt) && portaComoInt > 1024;
-
     }
-
-
 
     class ExpressionVariables
     {
@@ -23,23 +21,24 @@ namespace CSharp73_novidades
         public static void TestaExpressionVariables()
         {
             var idadeComoTexto = "15";
-
             
             if(int.TryParse(idadeComoTexto, out int idade) && idade > 18)
             {
                 Console.WriteLine("Voce pode entrar");
             }
-
-
-
-
-
         }
 
+        public static void RegistroDeAlunos()
+        {
+            var registroAlunos = new string[] {
+                "1110651", "1020651", "1110600",
+                "1310800", "211060T", "011060W"
+            };
 
-
-
-
-
+            var alunosValidos =
+                from ra in registroAlunos
+                where int.TryParse(ra, out int raComoInt) && raComoInt > 1000
+                select ra + " é válido";
+        }
     }
 }
