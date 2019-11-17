d4647f48ae5c647abe2358a6c70fe7a27696d842
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2019-11-17 13:32:06 -0300

    before video 2.1

---
+++ src/CSharp73_novidades/ReatribuicaoEmRefs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp73_novidades
+{
+    class ReatribuicaoEmRefs
+    {
+        public static void TestaRefs()
+        {
+            var numeros = new [] { 1, 3, 7, 15, 31, 1023, 63, 127, 255, 511 };
+            EscreverNumeros(numeros);
+        }
+
+        private static int ObterMaiorValor(int[] numeros)
+        {
+            var maior = numeros[0];
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                }
+            }
+
+            return maior;
+        }
+
+        private static void EscreverNumeros(int[] numeros) =>
+            Console.WriteLine(string.Join(", ", numeros));
+    }
+}
