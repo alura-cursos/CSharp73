109aa81023096f4930d595148e29b85b3be8913d
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2019-12-16 22:14:33 -0300

    Video 5.1

---
+++ src/CSharp73_novidades/ResolucaoDeSobrecargas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp73_novidades
+{
+    class DeInstanciaOuEstatico
+    {
+        public void TestaSobreCarga()
+        {
+            this.EscreveMensagem(null);
+            ResolucaoDeSobrecargas.EscreveMensagem(null);
+        }
+
+        public void EscreveMensagem(StringBuilder stringBuilder)
+        {
+            Console.WriteLine(stringBuilder);
+        }
+
+        public static void EscreveMensagem(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
